16c1f3173bd478f7f80c7890ae7ccbf8b3df9a5b
Nitipong Kadsritalee <nitipongkadsritalee01@gmail.com>
2026-02-19 08:54:52 +0700

    Arrange form page

--- warrant_form/forms.py
+++ warrant_form/forms.py
@@ -340,17 +340,17 @@ class MainAWISForm(forms.ModelForm):
             }),
             'scene': forms.Textarea(attrs={
                 'rows': 5,
-                'cols': 81,
+                'cols': 135,
                 'style':'resize:none;'
             }), 
             'act': forms.Textarea(attrs={
                 'rows': 5,
-                'cols': 90,
+                'cols': 135,
                 'style':'resize:none;'
             }),
             'charge': forms.Textarea(attrs={
                 'rows': 5,
-                'cols': 67,
+                'cols': 135,
                 'style':'resize:none;'
             }),
             "court_code": forms.HiddenInput(attrs={
