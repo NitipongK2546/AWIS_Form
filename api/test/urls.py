9556d5a8830e24ec1c4cb72737bbea2b505c9633
Nitipong Kadsritalee <nitipongkadsritalee01@gmail.com>
2026-03-11 10:41:42 +0700

    Add button... forgot

--- api/test/urls.py
+++ api/test/urls.py
@@ -6,4 +6,5 @@ app_name = "test"
 urlpatterns = [
     path("health/", endpoints.health_check, name="health"),
     path("token/", endpoints.fetch_token, name="login"),
+    path("check_req/", endpoints.fetch_token, name="check_reqform"),
 ]
