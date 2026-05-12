f7f210cdaea8cf8b72c1cb7db3d575b82d79ccd3
Nitipong Kadsritalee <nitipongkadsritalee01@gmail.com>
2026-05-12 15:42:42 +0700

    Add webhook endpoint to delete user access

--- _log_utils/file_logger.py
+++ _log_utils/file_logger.py
@@ -192,6 +192,14 @@ def _createLog(
 ########################################################################
 
 def createNormalLog(request : HttpRequest, action : AccessType, system : PermissionList, access_info : dict = None, remark : str = None, user_bypass : UserDataModel = None):
+    """
+    access_info\n
+    Either check the models for function of getLogInfoDict()\n
+    Or the input is:\n
+    {
+        "message": "Message Here",
+    }
+    """
     
     log_obj = _createLog(request, action, system, "normal", access_info, remark, NORMAL_LOG, user_bypass)
 
@@ -205,6 +213,11 @@ def createNormalLog(request : HttpRequest, action : AccessType, system : Permiss
     return log_obj 
 
 def createAccessDeniedLog(request : HttpRequest, action : AccessType, system : PermissionList, denied_reason : dict = None, remark : str = None, user_bypass : UserDataModel = None):
+    """
+    access_info = {
+        "message": "Message Here",
+    }
+    """
 
     log_obj = _createLog(request, action, system, "denied", denied_reason, remark, NORMAL_LOG, user_bypass)
 
@@ -219,6 +232,11 @@ def createAccessDeniedLog(request : HttpRequest, action : AccessType, system : P
     
 
 def createErrorLog(request : HttpRequest, action : AccessType, system : PermissionList, error_reason : dict = None, remark : str = None, user_bypass : UserDataModel = None):
+    """
+    access_info = {
+        "message": "Message Here",
+    }
+    """
 
     log_obj = _createLog(request, action, system, "errors", error_reason, remark, NORMAL_LOG, user_bypass)
 
--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,9 @@ app_name = "api"
 
 urlpatterns = [
     path("v1/", include("api.v1.urls")),
-    path("test/", include("api.test.urls")),
     path("internal/", include("api.internal.urls")),
+
+    path("webhook/", include("api.webhook.urls")),
+
+    path("test/", include("api.test.urls")),
 ]
